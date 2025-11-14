09b71d42f369b122435b8348787daa46fe66efb6
najmusysyarif <njmsyrf124ri@gmail.com>
2025-11-14 14:50:58 +0800

    Add files via upload

---
+++ pert3.py
@@ -0,0 +1,13 @@
+# we will learn type data 
+# integer (bilangan bulat)
+# float (pecahan)
+
+x = 10 # integer
+y = 10,5 # float
+status = "single" # string
+kebenaran = "false" # boolean (true false)
+
+print("nilai dari x = ", x,"bertype data", type(x))
+print("nilai dari y = ", y,"bertype data", type(y))
+print("nilai dari status =", status,"bertype data", type(status))
+print("nilai dari kebenaran =", kebenaran,"bertype data", type(kebenaran))
