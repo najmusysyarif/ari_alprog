624c111f394aee8c5af87829aaba8a15fd2c5719
najmusysyarif <njmsyrf124ri@gmail.com>
2025-11-14 14:57:39 +0800

    Add files via upload

--- pert4.py
+++ pert4.py
@@ -1,10 +1,29 @@
-# input dari sendiri
+# mengambil inputan dari user
+
+nama = input("Masukkan nama anda :")
+print("selamat datang ", "Ari")
+print(type(nama))
+
+umur = int(input("Masukkan umur anda :"))
+print("umur anda sekarang ", "20")
+print(type(umur))
+
+TinggiBadan = float(input("Masukkan TinggiBadan anda :"))
+print("TinggiBadan anda sekarang ", "169.7")
+print(type(TinggiBadan))
+
+hidup = bool(input("Masukkan hidup anda :"))
+print("Hidup anda sekarang ", "dramatis :")
+print(type(hidup))
+
+# buatlah program dengan output sebagai berikut
+# masukkan nama anda
+# masukkan nim anda 
 
 nama = input("Masukkan nama anda :")
 print("selamat datang ", nama)
 print(type(nama))
 
 nim = int(input("Masukkan nim anda :"))
-print("umur anda sekarang ", nim)
+print("nim anda sekarang ", nim)
 print(type(nim))
-
