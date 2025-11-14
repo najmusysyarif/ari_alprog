207c531a655feae6ba1b2010bfd16e46a5d3757c
najmusysyarif <njmsyrf124ri@gmail.com>
2025-11-14 14:51:49 +0800

    Add files via upload

---
+++ pert4.py
@@ -0,0 +1,10 @@
+# input dari sendiri
+
+nama = input("Masukkan nama anda :")
+print("selamat datang ", nama)
+print(type(nama))
+
+nim = int(input("Masukkan nim anda :"))
+print("umur anda sekarang ", nim)
+print(type(nim))
+
