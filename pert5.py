57dec3cac40d3adc3c0d8a8ad2c4fa05db6c5dd9
najmusysyarif <njmsyrf124ri@gmail.com>
2025-11-28 15:45:18 +0800

    Add files via upload

---
+++ pert5.py
@@ -0,0 +1,41 @@
+# operasi aritmatika x : + -
+
+# operasi penjumlahan +
+a = 25
+b = 31
+hasil = a + b
+print(a, "+", b, "=", hasil)
+
+# operasi pengurangan -
+hasil = a - b
+print(a, "-", b, "=", hasil)
+
+# operasi pembagian /
+hasil = a / b
+print(a, "/", b, "=", hasil)
+
+# operasi perkalian *
+hasil = a * b
+print(a, "*", b, "=", hasil)
+
+# operasi pangkat **
+hasil = a ** b
+print(a, "**", b, "=", hasil)
+
+# operasi modulus % (sisa bagi)
+hasil = a % b
+print(a, "%", b, "=", hasil)
+
+# operasi floor division // (pembulatan perkalian)
+hasil = a // b
+print(a, "//", b, "=", hasil)
+
+# operasi gabungan aritmatika
+gabung = 12 - 6 / 2 * 5 ** 2 + 1 % 3 // 4
+print(gabung)
+
+# operasi prioritas
+# 1. ()
+# 2. pangkat akar
+# 3. x :
+# 4. + -
